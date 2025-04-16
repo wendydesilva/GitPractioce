5f38acb00c2063a852b38e45566814b1217e97a1
300390845 <300390845@N6105S19.ad.douglascollege.ca>
2025-04-16 10:08:48 -0700

    First commit

---
+++ GitDemoProjectWeek15/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GitDemoProjectWeek15 {
+}
---
+++ GitDemoProjectWeek15/src/test/test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Git demo project");
+		System.out.println("created by team leader");
+
+	}
+
+}
